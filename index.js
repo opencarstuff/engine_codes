99d63bf1d445b1c00526ed2d6dd067e52b395ad6
Matthias <matthias@quasthoffs.de>
2025-05-25 22:21:16 +0100

    Add X-Frame-Options header

--- index.js
+++ index.js
@@ -148,6 +148,7 @@ function render(template, options = {}) {
     return res
       .setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
       .setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+      .setHeader('X-Frame-Options', 'DENY')
       .render(
         template,
         {
